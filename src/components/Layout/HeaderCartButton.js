8246d12d312d19d65f3c8bbe215cc6aae6e14089
Ramakrishna <ramakrishnamb@palo-it.com>
2023-05-16 16:40:58 +0530

    chore: css fix for cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,16 +2,16 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 
-const HeaderCartButton = props =>{
- return <button>
-     <button className={classes.button}>
+const HeaderCartButton = props => {
+  return (
+    <button className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>3</span>
     </button>
- </button>
+  );
 };
 
 export default HeaderCartButton;
