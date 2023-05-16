df466817fe5d21480b2a8fea9ad44b1898a6e1a6
Ramakrishna <ramakrishnamb@palo-it.com>
2023-05-16 17:07:16 +0530

    feat: Meals list added

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvaliableMeals";
+import { Fragment } from "react";
+
+const Meals = () => {
+    return (<Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+    </Fragment>
+    );
+};
+
+export default Meals;
