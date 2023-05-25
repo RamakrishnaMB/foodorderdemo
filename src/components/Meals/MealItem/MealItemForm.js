b41252997d83b29125e6c0c3da5684d99f7e2cf9
Ramakrishna <ramakrishnamb@palo-it.com>
2023-05-25 13:35:03 +0530

    feat: Meal Item Add form added with input

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,19 @@
+import Input from '../../UI/Input';
+import classess from './MealItemForm.module.css'
+
+const MealItemFrom = props => {
+    return <form className={classess.form}>
+        <Input label="Amount" input={{
+            id: 'amount',
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1'
+        }} />
+        <button>+ Add</button>
+    </form>
+
+};
+
+export default MealItemFrom;
